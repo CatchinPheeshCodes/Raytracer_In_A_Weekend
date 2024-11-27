fe831b6197787ef6151bf0c45e73fcce22c7984b
Travis Williams <catchinpheeshcodes@gmail.com>
2024-11-26 18:34:22 -0700

    modified to use vec3 and color header files.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 //This is the main file for my raytracer in a weekend project.
+
+//Include header files.
+#include "vec3.h"
+#include "color.h"
+
 #include <iostream>
+
 int main () {
     //image boundaries.
     int img_width = 256;
@@ -12,15 +18,10 @@ int main () {
         //Counter for remaining lines to be processed. j = current line. remaining = total lines - j.
         std::clog << "\rScanlines remaining: " << (img_height - j) << ' ' << std::flush;
         for(int i = 0; i < img_width; ++i) {
-            auto r = double(i) / (img_width - 1);
-            auto g = double(j) / (img_height - 1);
-            auto b = 0.0;
-
-            int ir = int(255.000 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            //Define pixel color.
+            auto pixel_color = color(double(i)/(img_width - 1), double(j)/(img_height - 1), 0);
+            //Write out pixel color.
+            write_color(std::cout, pixel_color);
         }
     } 
     std::clog << "\rDone.                  \n";
