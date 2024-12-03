8262d650891599c142abdd34105be126f081f1dd
Travis Williams <catchinpheeshcodes@gmail.com>
2024-12-03 14:05:27 -0700

    adjusted hit_sphere to return a double between -1 & 1. adjusted ray_color to return a sphere colored according to its normal vectors.

--- main.cpp
+++ main.cpp
@@ -8,20 +8,30 @@
 #include <iostream>
 
 //Determines if the ray interesects our simple sphere.
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 orig_c = center - r.origin();
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), orig_c);
     auto c = dot(orig_c, orig_c) - radius * radius;
     auto discriminant = b * b - 4 * a * c;
-    return(discriminant >= 0);
+    //each component between -1 and 1.
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        return(-b - std::sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 //returns the color for a given scene ray.  
 color ray_color(const ray& r) {
-    if(hit_sphere(point3(0, 0, -1), 0.5, r))
-        return color(1, 0, 0);
-        
+    //colors sphere according to its normal vectors.
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+    //blue gradient.
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
