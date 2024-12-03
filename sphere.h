f0dd1089cd9a98161e8b670c63c0ee15f863c299
Travis Williams <catchinpheeshcodes@gmail.com>
2024-12-03 16:59:38 -0700

    added surface side determination to sphere class.

--- sphere.h
+++ sphere.h
@@ -31,7 +31,8 @@ class sphere : public hittable {
             //Update hit record variables. See hittable.h for t, p and normal.
             rec.t = root;
             rec.p = r.at(rec.t);
-            rec.normal = (rec.p - center) / radius;
+            vec3 outward_normal = (rec.p - center) / radius;
+            rec.set_face_normal(r, outward_normal);
 
             return true;
         }
