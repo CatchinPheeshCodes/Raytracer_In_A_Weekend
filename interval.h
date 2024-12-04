0861ef270a67d326da51162ae72a28f3ad8e3e6e
Travis Williams <catchinpheeshcodes@gmail.com>
2024-12-04 14:39:18 -0700

    initial commit. header file for intervals.

---
+++ interval.h
@@ -0,0 +1,15 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+#include 'rtweekend.h'
+
+class interval {
+    public:
+        double min, max;
+
+        interval() : min(+infinity), max(-infinity) {}
+        
+
+};
+
+#endif
