33427867e54eb9b43a084cd799e4327b633cd480
Travis Williams <catchinpheeshcodes@gmail.com>
2024-12-08 16:24:56 -0700

    saved file.

--- material.h
+++ material.h
@@ -63,8 +63,7 @@ class dielectric : public material {
     private:
         //Refractive index in vacuum or air, or the ratio of the material's refractive index
         //over the refractive index of the enclosing media.
-        double refraction_index;
-        
+        double refraction_index;     
 };
 
 #endif
